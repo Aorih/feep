152bfdddf2a4ac52f1bbe0aea9765bf302f0171d
OFIDONA\sferreca <sferreca@mercadona.es>
2022-11-30 13:33:23 +0100

    feat: Added function to edit user description

--- src/app/feed/profile-card/profile-card.component.ts
+++ src/app/feed/profile-card/profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileCardComponent implements OnInit {
 
+  onEdit: boolean = false;
+  descriptionText: string | null = "About me...";
+  placeholder: string | null = "About me..."
+  @ViewChild('textArea') textArea!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  
+  editDescription(){
+    this.onEdit = true;
+    setTimeout(() => {
+      this.textArea.nativeElement.focus();
+    }, 50);
+  }
+
+  setDescription(){
+    const txtArea = this.textArea.nativeElement;
+    const text = txtArea.value !== "" ? txtArea.value : "About me..."
+
+    this.descriptionText = text;
+    this.placeholder = text;
+    this.onEdit = false;
   }
 
 }
