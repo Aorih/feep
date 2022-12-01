6aa06e195091ce8e3e334cd8d89b9098952247b1
OFIDONA\sferreca <sferreca@mercadona.es>
2022-12-01 09:27:37 +0100

    feat: Image selector for banner

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  @ViewChild('banner') banner!: ElementRef;
+  url: any = "default";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeBanner(event:any){
+
+    console.log("entro al metodo")
+    if(!event.target.files[0] || event.target.files[0].length == 0){
+      console.log("entro al primer if")
+      return;
+    }
+
+    let mimeType = event.target.files[0].type;
+    if(mimeType.match(/image\/*/) == null){
+      console.log("entro al segundo if")
+      return;
+    }
+
+    let reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (_event) => {
+      this.url = reader.result;
+      console.log(this.url);
+      this.banner.nativeElement.style.backgroundImage='url(' + this.url + ')';
+    }
+    
   }
 
 }
